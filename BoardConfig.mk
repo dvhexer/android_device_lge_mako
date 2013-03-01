92339f078389fbe4047c383624ab1f189baf0ad5
dvhexer <dvhexer@gmail.com>
2013-02-28 23:38:23 -0500

    support for various Optimus G devices

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,8 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
+TARGET_OTA_ASSERT_DEVICE := mako,geeb,gee_a,e970,gee,geebus
+
 # Try to build the kernel
 #TARGET_KERNEL_CONFIG := mako_defconfig
 TARGET_KERNEL_CONFIG := mako_j1a_defconfig
